47ec9de72b89c7772150d59b4c0f40edeed1529b
Erjon Avdija <116682750+tomsonstreatval@users.noreply.github.com>
2023-06-01 21:06:54 +0200

    navbar correction

--- ProPunes/resources/views/Aplikimet/create.blade.php
+++ ProPunes/resources/views/Aplikimet/create.blade.php
@@ -1,39 +1,91 @@
 @extends('Aplikimet.layout')
 
 @section('content')
+<style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+        * {
+            font-family: 'Montserrat', sans-serif;
+        }
+        .navbar .getstarted {
+            max-width: 100%;
+            background: #106eea;
+            margin-left: 30px;
+            border-radius: 4px;
+            font-weight: 400;
+            color: #fff;
+            text-decoration: none;
+            padding: .5rem 1rem;
+            line-height: 2.3;
+        }
+        .navbar-nav a {
+            font-size: 15px;
+            text-transform: uppercase;
+            font-weight: 500;
+        }
+        .navbar-light .navbar-brand {
+            color: #000;
+            font-size: 25px;
+            text-transform: uppercase;
+            font-weight: bold;
+            letter-spacing: 2px;
+        }
+        .navbar-light .navbar-brand:focus, .navbar-light .navbar-brand:hover {
+            color: #000;
+        }
+        .navbar-light .navbar-nav .nav-link {
+            color: #000;
+        }
+        .navbar-light .navbar-nav .nav-link:focus, .navbar-light .navbar-nav .nav-link:hover {
+            color: #000;
+        }
+        .w-100 {
+            height: 100vh;
+        }
+        .navbar-toggler {
+            padding: 1px 5px;
+            font-size: 18px;
+            line-height: 0.3;
+            background: #fff;
+        }
+        
+        @media only screen and (max-width: 767px) {
+        .navbar-nav {
+            text-align: center;
+        }
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" >
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <span class="text-warning"><b>PRO</b></span>PUNES</a>
-        <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-        <a class="nav-link" href="#services">Services</a>
-      </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#contact">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#jobs">Jobs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#login">Log In</a>
-                </li>
-            </ul>
+}
+</style>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <span class="text-success"><b>PRO</b></span>PUNES</a>
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('/Aplikimet/read') }}">About</a>
+                    </li>
+                    <li class="nav-item">
+						<a class="nav-link" href="{{ url('/Aplikimet/create') }}">Services</a>
+					</li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('aplikimet.store') }}">Jobs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('/login') }}">Log in</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
 </nav>
-
 
 
 <div class="row">
