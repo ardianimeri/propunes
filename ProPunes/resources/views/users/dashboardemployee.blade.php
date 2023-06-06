a723745d84a973e733fa16b009e8786c86019731
Arianit Sadriu <arianitsa1@gmail.com>
2023-06-06 23:12:50 +0200

    employee dashboard frontend

---
+++ ProPunes/resources/views/users/dashboardemployee.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <span class="text-warning"><b>PRO</b></span>PUNES</a>
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+						<a class="nav-link" href="#services">Services</a>
+					</li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#jobs">Jobs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#login">Log In</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container" style="margin-top: 10%;">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item active" aria-current="page"><a href="#">Profile</a></li>
+          </nav>
+        <div class="row">
+    </div>
+    <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Emri i Kompanise</th>
+            <th scope="col">Mosha</th>
+            <th scope="col">Data</th>
+            <th scope="col">Vendi</th>
+            <th scope="col">Pozita</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td></td>
+            <td>20</td>
+            <td>28/05/2023</td>
+            <td>Ferizaj</td>
+            <td>Full Stack Web</td>
+            <td><a class="btn btn-dark" href="#" role="button">Shiko</a></td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Kompania 2</td>
+            <td>20</td>
+            <td>28/05/2023</td>
+            <td>Ferizaj</td>
+            <td>Full Stack Web</td>
+            <td><a class="btn btn-dark" href="#" role="button">Shiko</a></td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Kompania 3</td>
+            <td>20</td>
+            <td>28/05/2023</td>
+            <td>Ferizaj</td>
+            <td>Full Stack Web</td>
+            <td><a class="btn btn-dark" href="#" role="button">Shiko</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
