3fa0df10e00543f499c4f40648660f09a48d49c2
ardianimeri <ardiani_m2003@hotmail.com>
2023-06-14 02:00:16 +0200

    making some changes on applications

--- ProPunes/resources/views/applications/index.blade.php
+++ ProPunes/resources/views/applications/index.blade.php
@@ -8,8 +8,7 @@
     @foreach ($applicants as $application)
         <div style="margin-top: 10%;">
             <h2>{{ $application->id }}</h2>
-            <h2>{{ $application->Titulli }}</h2>
-            <h2>{{ $application->Kategoria }}</h2>
+            <h2>{{ $application->name }}</h2>
         </div>
     @endforeach
 @endif
